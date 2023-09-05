90ab2c47a6fba249d8c7a2f6a2c0939238c7e920
Zach Schwartz <zachatorcodes@gmail.com>
2023-09-05 15:44:57 -0600

    Fixed serializers

--- app/serializers/client_serializer.rb
+++ app/serializers/client_serializer.rb
@@ -1,4 +1,4 @@
 class ClientSerializer < ActiveModel::Serializer
   attributes :id, :name, :age
-  has_many :memberships
+  has_many :gyms
 end
--- app/serializers/gym_serializer.rb
+++ app/serializers/gym_serializer.rb
@@ -1,4 +1,4 @@
 class GymSerializer < ActiveModel::Serializer
   attributes :id, :name, :address
-  has_many :memberships
+  has_many :clients
 end
